d2644ad84accd9f4ca5c305e3cae4d5b5436550b
ASUKAZO2 <aoomkung2547@gmail.com>
2026-04-25 18:05:06 +0700

    Add files via upload

--- calenda.sql
+++ calenda.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 31, 2026 at 01:18 PM
+-- Generation Time: Apr 25, 2026 at 01:06 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -30,27 +30,12 @@ SET time_zone = "+00:00";
 CREATE TABLE `attachments` (
   `id` int(11) NOT NULL,
   `user_id` int(11) DEFAULT NULL,
-  `file_url` text NOT NULL,
+  `file_url` varchar(255) DEFAULT NULL,
   `file_type` varchar(50) DEFAULT NULL,
   `note_id` int(11) DEFAULT NULL,
   `event_id` int(11) DEFAULT NULL,
   `uploaded_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `attachments`
---
-
-INSERT INTO `attachments` (`id`, `user_id`, `file_url`, `file_type`, `note_id`, `event_id`, `uploaded_at`) VALUES
-(23, NULL, 'uploads/1774955294_0_ระบบรับคำร้องแก้แบบฟอร์ม.pdf', 'pdf', 16, 16, '2026-03-31 11:08:14'),
-(24, NULL, 'uploads/1774955294_1_template โครงงานสหกิจศึกษาปรับ 1 คอลัมน์(1).pdf', 'pdf', 16, 16, '2026-03-31 11:08:14'),
-(25, NULL, 'uploads/1774955294_2_บทความสหกิจศึกษา นส.ธนิสร มโนรมย์.pdf', 'pdf', 16, 16, '2026-03-31 11:08:14'),
-(26, NULL, 'uploads/1774955489_0_ระบบรับคำร้องแก้แบบฟอร์ม.pdf', 'pdf', 17, 17, '2026-03-31 11:11:29'),
-(27, NULL, 'uploads/1774955489_1_template โครงงานสหกิจศึกษาปรับ 1 คอลัมน์(1).pdf', 'pdf', 17, 17, '2026-03-31 11:11:29'),
-(28, NULL, 'uploads/1774955489_2_บทความสหกิจศึกษา นส.ธนิสร มโนรมย์.pdf', 'pdf', 17, 17, '2026-03-31 11:11:29'),
-(29, NULL, 'uploads/1774955489_3_ข้อแนะนำในบทความสหกิจศึกษา.pdf', 'pdf', 17, 17, '2026-03-31 11:11:29'),
-(31, NULL, 'uploads/1774955587_0_ระบบรับคำร้องแก้แบบฟอร์ม.pdf', 'pdf', 19, 19, '2026-03-31 11:13:07'),
-(32, NULL, 'uploads/1774955765_0_edit_ระบบรับคำร้องแก้แบบฟอร์ม.pdf', 'pdf', 16, 16, '2026-03-31 11:16:05');
 
 -- --------------------------------------------------------
 
@@ -62,6 +47,8 @@ CREATE TABLE `events` (
   `id` int(11) NOT NULL,
   `user_id` int(11) DEFAULT NULL,
   `title` varchar(255) NOT NULL,
+  `content` text DEFAULT NULL,
+  `color` varchar(20) DEFAULT NULL,
   `start_time` datetime NOT NULL,
   `end_time` datetime NOT NULL,
   `note_id` int(11) DEFAULT NULL
@@ -71,11 +58,8 @@ CREATE TABLE `events` (
 -- Dumping data for table `events`
 --
 
-INSERT INTO `events` (`id`, `user_id`, `title`, `start_time`, `end_time`, `note_id`) VALUES
-(16, NULL, 'ฟฟฟฟ', '2026-04-01 00:00:00', '2026-04-01 23:59:59', 16),
-(17, NULL, 'กกก', '2035-10-25 00:00:00', '2035-10-25 23:59:59', 17),
-(18, NULL, 'A', '2026-04-09 00:00:00', '2026-04-09 23:59:59', 18),
-(19, NULL, 'AAAA', '2026-04-12 00:00:00', '2026-04-12 23:59:59', 19);
+INSERT INTO `events` (`id`, `user_id`, `title`, `content`, `color`, `start_time`, `end_time`, `note_id`) VALUES
+(17, NULL, 'กกก', NULL, NULL, '2035-10-25 00:00:00', '2035-10-25 23:59:59', NULL);
 
 -- --------------------------------------------------------
 
@@ -90,16 +74,6 @@ CREATE TABLE `notes` (
   `content` text DEFAULT NULL,
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `notes`
---
-
-INSERT INTO `notes` (`id`, `user_id`, `title`, `content`, `updated_at`) VALUES
-(16, NULL, 'ฟฟฟฟ', 'ฟฟฟฟ', '2026-03-31 11:08:14'),
-(17, NULL, 'กกก', 'กกก', '2026-03-31 11:11:29'),
-(18, NULL, 'A', 'A', '2026-03-31 11:12:37'),
-(19, NULL, 'AAAA', 'AA', '2026-03-31 11:13:07');
 
 -- --------------------------------------------------------
 
@@ -156,19 +130,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `attachments`
 --
 ALTER TABLE `attachments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- AUTO_INCREMENT for table `events`
 --
 ALTER TABLE `events`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `notes`
 --
 ALTER TABLE `notes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `users`
